3091dca917116cb5fc8be1ccd882513e3a2d2e81
MushfiqPlabon <rahman.mushfiq2000@gmail.com>
2022-09-23 10:19:44 +0600

    fixed some typos

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Product from "./Product";
 
 let products = [
-  { name: `P1`, desc: `Lorem ipsum dolor`, price: 50, img: ``, href: `` },
-  { name: `P2`, desc: `Lorem ipsum dolor`, price: 20, img: ``, href: `` },
-  { name: `P3`, desc: `Lorem ipsum dolor`, price: 60, img: ``, href: `` },
-  { name: `P4`, desc: `Lorem ipsum dolor`, price: 70, img: ``, href: `` },
+  { name: `P1`, desc: `Lorem ipsum dolor`, price: 50, img: `0`, href: `0` },
+  { name: `P2`, desc: `Lorem ipsum dolor`, price: 20, img: `0`, href: `0` },
+  { name: `P3`, desc: `Lorem ipsum dolor`, price: 60, img: `0`, href: `0` },
+  { name: `P4`, desc: `Lorem ipsum dolor`, price: 70, img: `0`, href: `0` },
 ];
 
 const ProductList = () => {
   return (
     <>
       <div id="art">
-        <h1 className="text-4xl text-center font-bold tracking-tight text-gray-900">
+        <h1 className="text-4xl text-center font-bold tracking-tight text-gray-900 p-9 m-6 pt-8">
           Would you like to purchase some of my art?
         </h1>
         <div className="flex flex-wrap py-5 justify-around justify-items-center justify-self-center content-around items-center self-center place-content-around place-items-center place-self-center">
